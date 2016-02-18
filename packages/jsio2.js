241881a11580a81d964628214d0f426a40986af3
souparno majumder <souparno.majumder@gmail.com>
2016-02-18 20:25:27 +0530

    wrapping up the raw import statements of the sosurce files with the jsio function call before evaluting the code string

--- packages/jsio2.js
+++ packages/jsio2.js
@@ -17,7 +17,8 @@ function jsio(req, exportInto) {
             }
         },
         item = resolveImportRequest(req),
-        src = "(function (_) { with (_) {" + loadModule(item.from).src + "}});",
+        module = loadModule(item.from),
+        src = "(function (_) { with (_) {" + module.src + "}});",
         fn = eval(src);
 
     fn(args);
@@ -31,13 +32,26 @@ jsio.setCache = function(cache) {
     jsio.__srcCache = cache;
 };
 
+function loadModule(from) {
+    var module = jsio.__srcCache[from];
+    module.src = applyPreprocessors(module.src);
+    return module;
+};
+
 jsio.addCmd = function(fn) {
     jsio.__cmds.push(fn);
 };
 
-function loadModule(from) {
-    return jsio.__srcCache[from];
-}
+function applyPreprocessors(src) {
+    var importExpr = /^(\s*)(import\s+[^=+*"'\r\n;\/]+|from\s+[^=+"'\r\n;\/ ]+\s+import\s+[^=+"'\r\n;\/]+)(;|\/|$)/gm;
+    return src.replace(importExpr,
+        function(raw, p1, p2, p3) {
+            if (!/\/\//.test(p1)) {
+                return p1 + 'jsio(\'' + p2 + '\')' + p3;
+            }
+            return raw;
+        });
+};
 
 function resolveImportRequest(request) {
     var cmds = jsio.__cmds,
@@ -72,10 +86,10 @@ jsio.setCache({
         src: "exports = function (req) { console.log(req);}"
     },
     "calculator": {
-        src: "jsio('import print as print'); exports = {add: function (a, b) { print(a+b);}}"
+        src: "import print as print; exports = {add: function (a, b) { print(a+b);}}"
     },
     "app": {
-        src: "jsio('import calculator as calculator'); calculator.add(2, 3);"
+        src: "import calculator as calculator; calculator.add(2, 3);"
     }
 
 });
