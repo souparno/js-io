0a2dbf5ead70a646a9711e4a3e075532792367c1
souparno majumder <souparno.majumder@gmail.com>
2016-03-11 14:37:31 +0530

    testWithNode.js file renamed to jsio.js

---
+++ jsio.js
@@ -0,0 +1,13 @@
+var jsio = require('./packages/jsio');
+var compiler = jsio('import .packages.preprocessors.compiler');
+
+
+function run(imports) {
+  compiler.compile(imports);
+
+  compiler.generateSrc(function(src) {
+    console.log(src + "jsio('" + imports + "');");
+  });
+};
+
+run(process.argv[2]);
