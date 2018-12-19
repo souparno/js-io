9af60708aff226f8a651f7b6077cf6684d427477
souparno <souparno.majumder@gmail.com>
2018-12-19 15:32:40 +0530

    simicolon beautify

--- __tests__/test2/bar.js
+++ __tests__/test2/bar.js
@@ -3,4 +3,4 @@ import .foo as foo;
 exports = function () {
   foo.hello();
   console.log("hello from bar");
-}
+};
--- __tests__/test2/foo.js
+++ __tests__/test2/foo.js
@@ -2,7 +2,7 @@ import .bar as bar;
 
 exports.hello = function () {
   console.log("hello from foo");
-}
+};
 
 bar();
 
