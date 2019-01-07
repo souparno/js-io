a2282f2713eb132d620d09cd72f1d6ad981ed528
souparno <souparno.majumder@gmail.com>
2019-01-08 00:09:49 +0530

    use strict

--- packages/jsio.js
+++ packages/jsio.js
@@ -18,6 +18,7 @@
 // LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
+"use strict";
 var fs = require('fs');
 var jsio = (function init() {
     var util = {
