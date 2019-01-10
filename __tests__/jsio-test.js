26ba614240be59cdcba197179e97332a99bbbdd6
souparno <souparno.majumder@gmail.com>
2019-01-10 18:33:35 +0530

    test case descriptioins modified

--- __tests__/jsio-test.js
+++ __tests__/jsio-test.js
@@ -11,7 +11,7 @@ describe('jsio.js', () => {
     });
   });
   describe('test case #2', () => {
-    it('should handle self dependent jsio.js syntax', done => {
+    it('should handle recursive jsio.js dependencies', done => {
       shelljs.exec('node jsio.js ./__tests__/test2/app.js | node', { silent: true} ,(code, stdout, stderr) => {
         assert.equal(stdout, 5);
         done();
@@ -27,7 +27,7 @@ describe('jsio.js', () => {
     });
   });
   describe('test case #4', () => {
-    it('should handle regular javascript syntax', done => {
+    it('should handle regular es6 require', done => {
       shelljs.exec('node jsio.js ./__tests__/test4/app.js | node', { silent: true} ,(code, stdout, stderr) => {
         assert.equal(stdout, 5);
         done();
@@ -35,7 +35,7 @@ describe('jsio.js', () => {
     });
   });
   describe('test case #5', () => {
-    it('should handle self dependent javascript syntax', done => {
+    it('should handle recursive es6 dependencies', done => {
       shelljs.exec('node jsio.js ./__tests__/test5/app.js | node', { silent: true} ,(code, stdout, stderr) => {
         assert.equal(stdout, 5);
         done();
@@ -43,7 +43,7 @@ describe('jsio.js', () => {
     });
   });
   describe('test case #6', () => {
-    it('should handle self contained javascript syntax', done => {
+    it('module.exports should be able to get accesed withing the source itself', done => {
       shelljs.exec('node jsio.js ./__tests__/test6/app.js | node', { silent: true} ,(code, stdout, stderr) => {
         assert.equal(stdout, 5);
         done();
